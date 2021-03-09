d81f56f1b047e8855440bb269fec2b4a52bcf461
WeiLin <weilin_one@yeah.net>
2021-03-09 23:16:42 +0800

    config files

---
+++ server/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  Mysql: {
+    host: '127.0.0.1',
+    user: 'xxxxxxxxx',
+    password: 'xxxxxxxxx',
+    database: 'minChat'
+  },
+  Robot: {
+    baseUrl: 'https://oapi.dingtalk.com/robot/send',
+    accessToken: 'xxxxxxxxxx',
+    secret: 'xxxxxxxxx'
+  }
+}
